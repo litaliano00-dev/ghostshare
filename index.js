6ef3369304036cab4eb48a39be96d99ffd358e4c
litaliano00-dev <litaliano00.contact@gmail.com>
2025-11-02 21:01:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 "use strict";
-/**
- * Audited & minimal JS implementation of hash functions, MACs and KDFs. Check out individual modules.
- * @module
- * @example
-```js
-import {
-  sha256, sha384, sha512, sha224, sha512_224, sha512_256
-} from '@noble/hashes/sha2';
-import {
-  sha3_224, sha3_256, sha3_384, sha3_512,
-  keccak_224, keccak_256, keccak_384, keccak_512,
-  shake128, shake256
-} from '@noble/hashes/sha3';
-import {
-  cshake128, cshake256,
-  turboshake128, turboshake256,
-  kmac128, kmac256,
-  tuplehash256, parallelhash256,
-  k12, m14, keccakprg
-} from '@noble/hashes/sha3-addons';
-import { blake3 } from '@noble/hashes/blake3';
-import { blake2b, blake2s } from '@noble/hashes/blake2';
-import { hmac } from '@noble/hashes/hmac';
-import { hkdf } from '@noble/hashes/hkdf';
-import { pbkdf2, pbkdf2Async } from '@noble/hashes/pbkdf2';
-import { scrypt, scryptAsync } from '@noble/hashes/scrypt';
-import { md5, ripemd160, sha1 } from '@noble/hashes/legacy';
-import * as utils from '@noble/hashes/utils';
-```
- */
-throw new Error('root module cannot be imported: import submodules instead. Check out README');
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./definitions"), exports);
+__exportStar(require("./frameworks"), exports);
+__exportStar(require("./config"), exports);
+__exportStar(require("./logger"), exports);
+__exportStar(require("./project"), exports);
+__exportStar(require("./xml"), exports);
+__exportStar(require("./json"), exports);
+__exportStar(require("./plist"), exports);
+__exportStar(require("./properties"), exports);
+__exportStar(require("./strings"), exports);
+__exportStar(require("./xcconfig"), exports);
+__exportStar(require("./vfs"), exports);
+__exportStar(require("./util/plist"), exports);
 //# sourceMappingURL=index.js.map
